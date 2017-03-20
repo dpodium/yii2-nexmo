ec3f1b791704c4967297be27909f8d2c0c746e45
darren.ng <darren.ng@dpodium.com>
2017-03-20 17:51:59 +0800

    Add support for proxy.

--- components/NexmoManager.php
+++ components/NexmoManager.php
@@ -92,6 +92,9 @@ class NexmoManager
 
     protected function initClient()
     {
-        return new \Nexmo\Client(new \Nexmo\Client\Credentials\Basic($this->config['api.key'], $this->config['api.secret']));
+        $http_client = isset($this->proxy) ? \Http\Adapter\Guzzle6\Client::createWithConfig([
+                'proxy' => $this->proxy['host'] . ':' . $this->proxy['port'],
+            ]) : null;
+        return new \Nexmo\Client(new \Nexmo\Client\Credentials\Basic($this->config['api.key'], $this->config['api.secret']), array(), $http_client);
     }
 }
